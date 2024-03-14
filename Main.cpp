ea082591b906d4baf7812bdffbd174104e13ba77
Jonah Barrera <116769667+jxnah7@users.noreply.github.com>
2024-03-14 18:49:03 -0500

    Create Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int main() {
+    for (int i = 1; i <= 100; i++)
+        if (i % 3 == 0) {
+            std::cout << "Fizz\n";
+        }
+        else if (i % 5 == 0) {
+            std::cout << "Buzz\n";
+        }
+        else if (i % 3 == 0 && i % 5 == 0) {
+            std::cout << "FizzBuzz\n";
+        }
+        else {
+            std::cout << i << "\n";
+
+        }
+
+}
+
+/*
+you have to write a program that prints the numbers from 1 to 100. 
+However, for multiples of 3, you should print "Fizz" instead of the number, and for multiples of 5, you should print "Buzz" instead of the number.
+For numbers which are multiples of both 3 and 5, you should print "FizzBuzz"
+*/
