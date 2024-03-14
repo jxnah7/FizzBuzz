ce187ac78dc44a51ed3451f2d615ecaadc451098
Jonah Barrera <116769667+jxnah7@users.noreply.github.com>
2024-03-14 18:53:08 -0500

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -11,9 +11,6 @@ int main() {
         else if (i % 5 == 0) {
             std::cout << "Buzz\n";
         }
-        else if (i % 3 == 0 && i % 5 == 0) {
-            std::cout << "FizzBuzz\n";
-        }
         else {
             std::cout << i << "\n";
         }
